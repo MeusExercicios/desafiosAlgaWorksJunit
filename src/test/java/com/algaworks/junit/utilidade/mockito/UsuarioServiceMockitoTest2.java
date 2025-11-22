d454c5442400562545cf197916084809d127f343
Vitor <vitorcostaofficial@gmail.com>
2025-11-22 18:46:31 -0300

    feat: uso de annotations com o mockito

--- src/test/java/com/algaworks/junit/utilidade/mockito/UsuarioServiceMockitoTest2.java
+++ src/test/java/com/algaworks/junit/utilidade/mockito/UsuarioServiceMockitoTest2.java
@@ -1,46 +1,39 @@
-    package com.algaworks.junit.utilidade.mockito;
+package com.algaworks.junit.utilidade.mockito;
 
-    import com.algaworks.junit.utilidade.stub.RepositorioUsuario;
-    import com.algaworks.junit.utilidade.stub.Usuario;
-    import com.algaworks.junit.utilidade.stub.UsuarioService;
-    import org.junit.jupiter.api.DisplayNameGeneration;
-    import org.junit.jupiter.api.DisplayNameGenerator;
-    import org.junit.jupiter.api.Test;
+import com.algaworks.junit.utilidade.stub.RepositorioUsuario;
+import com.algaworks.junit.utilidade.stub.Usuario;
+import com.algaworks.junit.utilidade.stub.UsuarioService;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-    import static org.junit.jupiter.api.Assertions.assertEquals;
-    import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
-    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-    public class UsuarioServiceMockitoTest {
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class UsuarioServiceMockitoTest2 {
 
-        @Test
-        void dado_um_usuario_quando_receber_id_entao_retorne_email(){
-            RepositorioUsuario repoMock = mock(RepositorioUsuario.class);
+    @Mock
+    RepositorioUsuario repoMock;
 
-            when(repoMock.buscarPorId(1L)).thenReturn(new Usuario(1L,"Carlin","carlin@gmail.com"));
-            when(repoMock.buscarPorId(2L)).thenThrow(new RuntimeException("Erro no banco"));
+    @InjectMocks
+    UsuarioService service;
 
-            UsuarioService service = new UsuarioService(repoMock);
+    @Test
+    void dado_id_quando_tiver_no_banco_entao_deve_retornar_o_email() {
+        when(repoMock.buscarPorId(1L)).
+                thenReturn(new Usuario(1L, "Carlin", "carlin@gmail.com"));
 
-            String email = service.obterEmail(1L);
+        String email = service.obterEmail(1L);
 
-            assertEquals("carlin@gmail.com",email);
+        assertEquals("carlin@gmail.com", email);
 
-            verify(repoMock).buscarPorId(1L);
-        }
+        verify(repoMock).buscarPorId(1L);
 
-
-        @Test
-        void deve_retornar_null_quando_o_usuario_nao_existir(){
-            RepositorioUsuario repoMock = mock(RepositorioUsuario.class);
-            when(repoMock.buscarPorId(999L)).thenReturn(null);
-            UsuarioService service = new UsuarioService(repoMock);
-            String email = service.obterEmail(999L);
-
-            assertEquals(null,email);
-
-            verify(repoMock).buscarPorId(999L);
-
-
-        }
     }
+}
